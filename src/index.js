23b94a879f8d6ec238bfeb82743e2bc2aa512e44
FrancoRosa <km115.franco@gmail.com>
2020-10-14 15:55:20 -0500

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 const App = () => (
   <h1>Working React!</h1>
-)
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-
