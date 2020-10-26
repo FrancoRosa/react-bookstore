0401cf9110ce4ee528655ad4e82d4bb64656e6da
FrancoRosa <km115.franco@gmail.com>
2020-10-26 13:28:35 -0500

    Add navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 // eslint-disable-next-line import/no-named-as-default
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
-
+import NavBar from '../components/Navbar';
 const App = () => (
   <div>
-    <BooksForm />
+    <NavBar />
     <BooksList />
+    <BooksForm />
   </div>
 );
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => (
+  <nav>
+    Bookstore CMS
+  </nav>
+);
+
+export default NavBar;
